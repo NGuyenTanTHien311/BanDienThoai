204db9a8c1b3ac1195c9d574056506b3055421b6
Nguyen Tan Thien <Nguyentanthien3101@email.com>
2023-11-04 11:22:37 +0700

    cập nhật

--- BanDienThoai/Controllers/HomeController.cs
+++ BanDienThoai/Controllers/HomeController.cs
@@ -80,5 +80,7 @@ namespace BanDienThoai.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+
     }
 }
